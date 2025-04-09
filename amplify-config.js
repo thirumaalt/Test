be7ae32a925b3bfd4f613354d6ed79553465314d
Thirumaal T <67221923+thirumaalt@users.noreply.github.com>
2025-04-09 09:24:13 +0530

    Add files via upload

---
+++ amplify-config.js
@@ -0,0 +1,17 @@
+// amplify-config.js
+window.aws_amplify_config = {
+  Auth: {
+    region: 'ap-south-1',                // Your region (for example: ap-south-1)
+    userPoolId: 'ap-south-1_AWVfn9P3b',     // Your manually created User Pool ID
+    userPoolWebClientId: '7do93ckljrsk5s9tbgtc0dv553', // Your App Client ID (without secret)
+    mandatorySignIn: true,               // Force sign in for protected pages
+  },
+  Storage: {
+    AWSS3: {
+      bucket: 'profile-pic-fbclone',        // Your S3 Bucket name (for profile and feed images)
+      region: 'ap-south-1',              // The same region as above
+    },
+  },
+};
+
+aws_amplify.Amplify.configure(window.aws_amplify_config);
